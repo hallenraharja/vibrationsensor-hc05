b3a8fd5788cb03554d5fe1d8966f76dbfed8661d
Hallen Raharja <hallenraharja@gmail.com>
2019-09-18 09:14:36 +0700

    Add files via upload

---
+++ HC-05.ino
@@ -0,0 +1,31 @@
+int nilaiSensor;
+int pinSensor;
+int bunyi;
+void setup() {
+  // put your setup code here, to run once:
+  pinSensor=7;
+  bunyi=13;
+  Serial.begin(9600);
+  pinMode(pinSensor, INPUT);
+  pinMode(bunyi, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Serial.println("Hallen");
+  if (digitalRead(pinSensor)==HIGH)
+  {
+    nilaiSensor=1;
+    digitalWrite(bunyi, HIGH);
+  }
+  if (digitalRead(pinSensor)==LOW)
+  {
+    nilaiSensor=0;
+    digitalWrite(bunyi, LOW);
+    }
+  Serial.println("Nilai Sensor Adalah...");
+  Serial.println(nilaiSensor);
+  delay(100);
+  
+}
+
